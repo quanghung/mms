5ce4db68d29adddd659ee42858f330b5bfe387e8
NghiemThai <nghiem.hong.thai@framgia.com>
2013-12-31 14:15:44 +0700

    edit user, add edit list position

---
+++ app/controllers/admin/positions_controller.rb
@@ -0,0 +1,57 @@
+class Admin::PositionsController < ApplicationController
+  before_action :signed_in_admin
+  
+  def index
+    @positions = Position.all.paginate page: params[:page]
+  end
+
+  def new
+    @position = Position.new
+  end
+
+  def create
+    position = Position.new position_params
+    if position.save
+      flash[:success] = I18n.t ".messages.add_user.success"
+      redirect_to admin_positions_path
+    else
+      flash[:success] = I18n.t ".messages.add_user.unsuccess"
+      render "new"
+    end
+  end
+
+  def edit
+    @position = Position.find params[:id]
+  end
+
+  def update
+    position = Position.find params[:id]
+    if position.update_attributes position_params
+      flash[:success] = I18n.t ".messages.update.success"
+      redirect_to admin_positions_path
+    else
+      flash[:success] = I18n.t ".messages.update.unsuccess"
+      redirect_to "edit"
+    end
+  end
+
+  def destroy
+    position = Position.find params[:id]
+    if position.update_attributes! active_flag: 0
+      flash[:success] = I18n.t ".messages.delete_user.success"
+      users = User.users_has_position position
+      users.each do |user|
+        user.not_destroy = true
+        user.update_attributes! active_flag: 0
+      end
+    else
+      flash[:success] = I18n.t ".messages.delete_user.unsuccess"
+    end
+    redirect_to admin_positions_path
+  end
+
+  private
+  def position_params
+    params.require(:position).permit :name, :abbreviation
+  end
+end
